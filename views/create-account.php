517168954bcbde52508651acae96beebeebe5a38
Rawal Singh <rawaldeepsingh26@gmail.com>
2019-03-25 16:21:14 +0100

    login system with token

--- views/create-account.php
+++ views/create-account.php
@@ -31,13 +31,20 @@ if (isset($_POST['createaccount'])) {
 <!doctype html>
 <html lang="en">
   <head>
-  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
-
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Material Design for Bootstrap fonts and icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/styles/mdb.min.css">
     <title>Register new user</title>
   </head>
   <body>
-  
-<form class="form-horizontal" action='' method="POST">
+<div class="container">
+<div class="col-lg-5 offset-lg-3">
+<form class="form-horizontal mx-auto my-5" action='' method="POST">
   <fieldset>
     <div id="legend">
       <legend class="">Register</legend>
@@ -46,7 +53,7 @@ if (isset($_POST['createaccount'])) {
       <!-- Username -->
       <label class="control-label"  for="username">Username</label>
       <div class="controls">
-        <input type="text" id="username" name="username" placeholder="" class="input-xlarge">
+        <input type="text" id="username" name="username" placeholder="" class="form-control mb-4 input-xlarge">
         <p class="help-block">Username can contain any letters or numbers, without spaces</p>
       </div>
     </div>
@@ -55,7 +62,7 @@ if (isset($_POST['createaccount'])) {
       <!-- E-mail -->
       <label class="control-label" for="email">E-mail</label>
       <div class="controls">
-        <input type="text" id="email" name="email" placeholder="" class="input-xlarge">
+        <input type="text" id="email" name="email" placeholder="" class="form-control mb-4 input-xlarge">
         <p class="help-block">Please provide your E-mail</p>
       </div>
     </div>
@@ -64,7 +71,7 @@ if (isset($_POST['createaccount'])) {
       <!-- Password-->
       <label class="control-label" for="password">Password</label>
       <div class="controls">
-        <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+        <input type="password" id="password" name="password" placeholder="" class="form-control mb-4 input-xlarge">
         <p class="help-block">Password should be at least 6 characters</p>
       </div>
     </div>
@@ -74,12 +81,17 @@ if (isset($_POST['createaccount'])) {
     <div class="control-group">
       <!-- Button -->
       <div class="controls">
-      <button class="btn btn-success"name="createaccount">Create Account</button> 
+      <button class="btn btn-success btn-block my-4" name="createaccount">Create Account</button> 
       </div>
     </div>
   </fieldset>
 </form>
-<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</div>
+</div>
+<!-- Js magic below -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script><script src="../assets/js/mdb.min.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
    </body>
 </html>
--- views/login.php
+++ views/login.php
@@ -40,7 +40,6 @@ if(isset($_POST['login'])){
     <title>Document</title>
 </head>
 <body>
-<div class="row">
 <div class="container">
 <div class="col-lg-5 offset-lg-3">
 <form action="login.php" method="post" class="text-center border border-light p-5 mx-auto my-5">
@@ -48,10 +47,10 @@ if(isset($_POST['login'])){
     <p class="h4 mb-4">Sign in</p>
 
     <!-- Email -->
-    <input type="text" id="defaultLoginFormUsername" class="form-control mb-4" name="username" placeholder="Username">
+    <input type="text" id="defaultLoginFormUsername" class="form-control mb-4 input-xlarge" name="username" placeholder="Username">
 
     <!-- Password -->
-    <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" name="password" placeholder="Password">
+    <input type="password" id="defaultLoginFormPassword" class="form-control mb-4 input-xlarge" name="password" placeholder="Password">
 
     <div class="d-flex justify-content-around">
         <div>
@@ -77,7 +76,6 @@ if(isset($_POST['login'])){
 
 
 </form>
-</div>
 </div>
 </div>
 <!-- Js magic below -->
