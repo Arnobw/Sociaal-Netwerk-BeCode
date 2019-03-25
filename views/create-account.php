226c0da46c66899e03db397ccbfa3919a72d4771
Carolien <heymanscarolien@gmail.com>
2019-03-25 16:25:04 +0100

    test

--- views/create-account.php
+++ views/create-account.php
@@ -32,53 +32,45 @@ if (isset($_POST['createaccount'])) {
 <html lang="en">
   <head>
   <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
-
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/styles/mdb.min.css">
+    <link rel="stylesheet" href="../assets/styles/style.css">
     <title>Register new user</title>
   </head>
   <body>
-  
-<form class="form-horizontal" action='' method="POST">
-  <fieldset>
-    <div id="legend">
-      <legend class="">Register</legend>
-    </div>
-    <div class="control-group">
-      <!-- Username -->
-      <label class="control-label"  for="username">Username</label>
-      <div class="controls">
-        <input type="text" id="username" name="username" placeholder="" class="input-xlarge">
-        <p class="help-block">Username can contain any letters or numbers, without spaces</p>
-      </div>
-    </div>
- 
-    <div class="control-group">
-      <!-- E-mail -->
-      <label class="control-label" for="email">E-mail</label>
-      <div class="controls">
-        <input type="text" id="email" name="email" placeholder="" class="input-xlarge">
-        <p class="help-block">Please provide your E-mail</p>
-      </div>
-    </div>
- 
-    <div class="control-group">
-      <!-- Password-->
-      <label class="control-label" for="password">Password</label>
-      <div class="controls">
-        <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
-        <p class="help-block">Password should be at least 6 characters</p>
-      </div>
-    </div>
- 
-  
- 
-    <div class="control-group">
-      <!-- Button -->
-      <div class="controls">
-      <button class="btn btn-success"name="createaccount">Create Account</button> 
-      </div>
-    </div>
-  </fieldset>
-</form>
+  <div class="container" id="register-form">
+  <!-- Default form register -->
+<form class="text-center border border-light p-5"action='' method="POST">
+
+<p class="h2 mb-6">Sign up</p>
+
+<div class="form-row mb-8">
+
+        <!-- username -->
+        <input type="text" id="username" name="username" class="form-control mb-4" placeholder="Username">
+    
+
+      
+
+<!-- E-mail -->
+<input type="email" id="email" name="email" class="form-control mb-4" placeholder="E-mail">
+
+<!-- Password -->
+<input type="password" id="password" name="password" class="form-control" placeholder="Password" aria-describedby="defaultRegisterFormPasswordHelpBlock">
+<small id="defaultRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
+    At least 8 characters and 1 digit
+</small>
+
+
+
+
+<!-- Sign up button -->
+<button class="btn btn-info my-4 btn-block" name="createaccount" type="submit">Create account</button>
+
+<hr>
+
+</div>
 <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
    </body>
