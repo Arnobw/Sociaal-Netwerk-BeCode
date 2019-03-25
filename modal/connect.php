70dc947a22d4a89bf93e7138e973afac199c9806
Rawal Singh <rawaldeepsingh26@gmail.com>
2019-03-25 15:42:37 +0100

    login system with token

---
+++ modal/connect.php
@@ -0,0 +1,39 @@
+<?php
+
+class DB {
+
+  // Properties
+  private static $dbHost = "127.0.0.1";
+	private static $dbName = "socialnetwork";
+	private static $dbUser = "root";
+	private static $dbPass = "";
+	private static $sharedPDO;
+	protected $pdo;
+
+  // Constructor
+  private static function connect() {
+
+		if(empty(self::$sharedPDO)) {
+			self::$sharedPDO = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUser, self::$dbPass);
+			self::$sharedPDO->exec("SET CHARACTER SET utf8");
+			self::$sharedPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			self::$sharedPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		}
+
+		return self::$sharedPDO;
+
+	}
+
+  // Methods
+  public static function query($query, $params = array()) {
+    $statement = self::connect()->prepare($query);
+    $statement->execute($params);
+    if (explode(' ', $query)[0] == 'SELECT') {
+    $data = $statement->fetchAll();
+    return $data;
+    }
+}
+
+}
+
+ ?>
---
+++ views/login.php
@@ -0,0 +1,88 @@
+<?php 
+include('../modal/connect.php');
+if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $pass = $_POST['password'];
+
+    if(DB::query('SELECT username FROM users WHERE username=:username', array (':username'=>$username)))
+    {
+        if(password_verify($pass, DB::query('SELECT password FROM users WHERE username=:username', array(':username'=>$username))[0]['password']))
+        {
+            echo 'Logged in';
+            $cstrong = True;
+            $token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+            $user_id = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$username))[0]['id'];
+            DB::query('INSERT INTO login_tokens VALUES (\'\', :token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$user_id));
+
+            
+        }else{
+            echo 'Incorrect Password!<br/>';
+        }
+
+    }else
+    {
+        echo 'user not registered';
+    }
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Material Design for Bootstrap fonts and icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/styles/mdb.min.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="row">
+<div class="container">
+<div class="col-lg-5 offset-lg-3">
+<form action="login.php" method="post" class="text-center border border-light p-5 mx-auto my-5">
+
+    <p class="h4 mb-4">Sign in</p>
+
+    <!-- Email -->
+    <input type="text" id="defaultLoginFormUsername" class="form-control mb-4" name="username" placeholder="Username">
+
+    <!-- Password -->
+    <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" name="password" placeholder="Password">
+
+    <div class="d-flex justify-content-around">
+        <div>
+            <!-- Remember me -->
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+                <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+            </div>
+        </div>
+        <div>
+            <!-- Forgot password -->
+            <a href="">Forgot password?</a>
+        </div>
+    </div>
+
+    <!-- Sign in button -->
+    <input class="btn btn-info btn-block my-4" name="login" value="login" type="submit">
+
+    <!-- Register -->
+    <p>Not a member?
+        <a href="">Register</a>
+    </p>
+
+
+</form>
+</div>
+</div>
+</div>
+<!-- Js magic below -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script><script src="../assets/js/mdb.min.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+</body>
+</html>
