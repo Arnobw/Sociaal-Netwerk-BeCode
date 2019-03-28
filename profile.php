5a63851cd7eda8657790c28640db83861be89735
Carolien <heymanscarolien@gmail.com>
2019-03-28 15:16:47 +0100

    mvc fail

--- profile.php
+++ profile.php
@@ -81,7 +81,7 @@ if (isset($_GET['username'])) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title><?php echo $username ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="assets/styles/main.css">
         <script src="main.js"></script>
 </head>
 <body>
